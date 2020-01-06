2fd1b9c2699be3865055f4df72cf041142ccc82b
Nate Montgomery <kilikalbright@hotmail.com>
2020-01-06 01:29:16 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -10,7 +10,7 @@ end
 def print_first_directors_movie_titles
   counter = 0
   while directors_database[0][:movies][counter]
-   p directors_database[0][:movies][counter][:title]
+   p directors_database[0][:movies][counter][:title]\n
   counter += 1 
   end
 end
